3c4017d6f8f176489e9f1758f88da7a96a188066
kiki <xiawucha0908@gmail.com>
2018-11-14 09:34:53 -0500

    education update

--- src/components/education.js
+++ src/components/education.js
@@ -4,17 +4,26 @@ class Education extends Component {
   render() {
     // $(".mdl-layout__content").css("background-color", "#000");
     return (
-      //   <div>
-
       <div>
-        Master of Science, GIS for Development and Environment, Clark University
-        <br />
-        Received in May 2016
-        <br />
-        Bachelor of Science, GIS, Shandong Agriculture University
-        <br /> Received in July 2014
+        <div className="education_block">
+          <img
+            src={require("../img/clarkuniversity.png")}
+            alt="clark University"
+          />
+          Master of Science, GIS for Development and Environment, Clark
+          University
+          <br />
+          Received in May 2016
+        </div>
+        <div className="education_block">
+          <img
+            src={require("../img/sd_agriculture.gif")}
+            alt="shandong agriculture"
+          />
+          Bachelor of Science, GIS, Shandong Agriculture University
+          <br /> Received in July 2014
+        </div>
       </div>
-      //   </div>
     );
   }
 }
